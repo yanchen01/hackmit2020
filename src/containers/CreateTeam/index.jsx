4ac2789cbe206e75ceee28ab564a5e66c38cd024
David Pham <davidpham@utexas.edu>
2020-09-19 02:01:01 -0500

    Add create team, uuid

--- src/containers/CreateTeam/index.jsx
+++ src/containers/CreateTeam/index.jsx
@@ -1,11 +1,86 @@
-import React from 'react';
+import React, { useState } from "react";
+import {
+  InputGroup,
+  Row,
+  FormControl,
+  Container,
+  Button,
+  FormGroup,
+} from "react-bootstrap";
+import { useForm } from "react-hook-form";
+import "react-datepicker/dist/react-datepicker.css";
 
 const CreateTeam = () => {
+  const onSubmit = (data) => {};
+
+  const { register, handleSubmit, watch, errors } = useForm();
+
   return (
-    <div>
-      <h1>Create Team</h1>
-    </div>
+    <section className="create-event-page m-xl-5">
+      <Container>
+        <h1>Create Team</h1>
+
+        <div className="mt-lg-3 mb-lg-3">
+          <label>Enter team name</label>
+          <InputGroup>
+            <FormControl
+              name="team"
+              ref={register({ min: 1, required: true, maxLength: 25 })}
+              placeholder="Team1"
+              aria-label="team"
+              aria-describedby="basic-addon1"
+            />
+          </InputGroup>
+          {errors.team && (
+            <span className="text-danger">
+              This field is required (1-25 characters)
+            </span>
+          )}
+        </div>
+
+        {/* conditionally based on current event's max team member size */}
+        <div className="mt-lg-3 mt-lg-3">
+          <label>Max members</label>
+          <InputGroup>
+            <FormControl
+              name="maxMembers"
+              type="number"
+              ref={register({ min: 2, max: 50, required: true })}
+              placeholder="4"
+              aria-label="Username"
+              aria-describedby="basic-addon1"
+            />
+          </InputGroup>
+          {errors.maxMembers && (
+            <span className="text-danger">This field is required</span>
+          )}
+        </div>
+
+        <div className="mt-lg-3 mt-lg-3">
+          <label>Current team size</label>
+          <InputGroup>
+            <FormControl
+              name="currentTeamSize"
+              type="number"
+              ref={register({ min: 2, max: 50, required: true })}
+              placeholder="4"
+              aria-label="usersPerTeam"
+              aria-describedby="basic-addon1"
+            />
+          </InputGroup>
+          {errors.currentTeamSize && (
+            <span className="text-danger">This field is required</span>
+          )}
+        </div>
+
+        <div className="mt-lg-3 mb-lg-3">
+          <Button onClick={handleSubmit(onSubmit)} variant="primary" size="lg">
+            Create Team
+          </Button>
+        </div>
+      </Container>
+    </section>
   );
-}
+};
 
 export default CreateTeam;
--- src/containers/Join/index.jsx
+++ src/containers/Join/index.jsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import { InputGroup, Row, FormControl, Container, Button } from "react-bootstrap";
+import React from "react";
+import {
+  InputGroup,
+  Row,
+  FormControl,
+  Container,
+  Button,
+} from "react-bootstrap";
 import { useForm } from "react-hook-form";
-import firebase from 'firebase';
-import './style.css';
+import firebase from "firebase";
+import "./style.css";
 
 const Join = () => {
   const onSubmit = (data) => {
     const { eventCode, name } = data;
-  }
+  };
 
   const { register, handleSubmit, watch, errors } = useForm();
 
   return (
-    <section className='join-event-page m-xl-5'>
+    <section className="join-event-page m-xl-5">
       <Container>
         <h1>Join Event</h1>
 
-        <div className='mt-lg-3 mb-lg-3'>
+        <div className="mt-lg-3 mb-lg-3">
           <label>Enter event code</label>
           <InputGroup>
             <FormControl
-              name='eventCode'
+              name="eventCode"
               ref={register({ min: 1, required: true, maxLength: 25 })}
               placeholder="hackmit2020"
               aria-label="eventcode"
@@ -28,15 +34,15 @@ const Join = () => {
             />
           </InputGroup>
         </div>
-        {errors.eventCode && <span className='text-danger'>This field is required</span>}
+        {errors.eventCode && (
+          <span className="text-danger">This field is required</span>
+        )}
 
-        <div className='mt-lg-3 mt-lg-3'>
-          <label>
-            What would you like your teammates to call you?
-          </label>
+        <div className="mt-lg-3 mt-lg-3">
+          <label>What would you like your teammates to call you?</label>
           <InputGroup>
             <FormControl
-              name='name'
+              name="name"
               ref={register({ min: 3, max: 50, required: true })}
               placeholder="Alex"
               aria-label="Username"
@@ -44,16 +50,18 @@ const Join = () => {
             />
           </InputGroup>
         </div>
-        {errors.name && <span className='text-danger'>This field is required</span>}
+        {errors.name && (
+          <span className="text-danger">This field is required</span>
+        )}
 
-        <div className='mt-lg-3 mb-lg-3'>
-          <Button onClick={handleSubmit(onSubmit)} variant='primary' size='lg'>
+        <div className="mt-lg-3 mb-lg-3">
+          <Button onClick={handleSubmit(onSubmit)} variant="primary" size="lg">
             Join
           </Button>
         </div>
       </Container>
     </section>
   );
-}
+};
 
 export default Join;
