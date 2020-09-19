0956c21a1623fbf29922be0968e0b9e875bc6f6c
David Pham <davidpham@utexas.edu>
2020-09-18 21:58:49 -0500

    Add pages

---
+++ src/containers/CreateEvent/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CreateEvent = () => {
+  return (
+    <div>
+      <h1>Create Event</h1>
+
+    </div>
+  );
+}
+
+export default CreateEvent;
--- src/containers/Join/index.jsx
+++ src/containers/Join/index.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import firebase from 'firebase';
 
 const Join = () => {
+  const onSubmit = () => {
+
+  }
+
   return (
-    <></>
+    <div>
+      <h1>Join Event</h1>
+      <form onSubmit={onSubmit}>
+        <label>
+          Enter event code
+          <input name="code" type="code" placeholder="hackmit2020" />
+        </label>
+        <label>
+          What would you like your teammates to call you?
+          <input name="name" type="name" placeholder="Alex" />
+        </label>
+      </form>
+    </div>
   );
 }
 
