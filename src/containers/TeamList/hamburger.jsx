826f447b9fa7daead3aa12bd3cc1bac5061a275e
Reid Miller <reid.miller@wisc.edu>
2020-09-19 10:43:27 -0500

    Remove unused code/imports

--- src/containers/TeamList/hamburger.jsx
+++ src/containers/TeamList/hamburger.jsx
@@ -7,15 +7,10 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import './index.css'
 
 // Drawer/HAMBURGER menu
-import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
-import DrawerButton from '@material-ui/core/Button';
 import List from '@material-ui/core/List';
-import Divider from '@material-ui/core/Divider';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
+
 
 const Hamburger = () => {
     // Hamburger
--- src/containers/TeamList/index.jsx
+++ src/containers/TeamList/index.jsx
@@ -2,35 +2,18 @@ import React, { useState } from 'react';
 import { Navbar, Container, Row, Col, ListGroup, Button } from 'react-bootstrap';
 import InfiniteScroll from 'react-infinite-scroll-component';
 import faker from 'faker'
-import { brotliCompress } from 'zlib';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import Hamburger from './hamburger.jsx';
 import './index.css'
 
 // Drawer/HAMBURGER menu
-import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
-import DrawerButton from '@material-ui/core/Button';
 import List from '@material-ui/core/List';
-import Divider from '@material-ui/core/Divider';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
 
 
 const TeamList = () => {
 
   // Hamburger
-  const useStyles = makeStyles({
-    list: {
-      width: 250,
-    },
-    fullList: {
-      width: 'auto',
-    },
-  });
-
   const [anchor, setAnchor] = useState(false);
 
   const toggleDrawer = (anchor, open) => (event) => {
