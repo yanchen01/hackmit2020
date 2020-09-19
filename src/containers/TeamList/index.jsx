3480455a97f5910c1e542a968747867aee81c78a
Reid Miller <reid.miller@wisc.edu>
2020-09-19 00:21:31 -0500

    Team List adding Teams

--- src/containers/TeamList/index.jsx
+++ src/containers/TeamList/index.jsx
@@ -1,16 +1,39 @@
 import React from 'react';
-import { Navbar, Container } from 'react-bootstrap';
+import { Navbar, Container, Row, Col } from 'react-bootstrap';
+
+class Team extends React.Component {
+  render() {
+    return(
+      <Container fluid>
+        <div className="rounded-pill border border-dark">
+        <Row>
+            <Col>Team Name</Col> 
+            <Col>3/4</Col>        
+        </Row>
+        <Row>
+            <Col>Description</Col>
+        </Row>
+        </div>
+      </Container>
+
+    );
+  }
+}
 
 const TeamList = () => {
   return (
     
     <div>
-  <Container>
-  <Navbar expand="lg" variant="light" bg="light">
-    <Navbar.Brand href="#">Navbar</Navbar.Brand>
-  </Navbar>
-</Container>
+      <Navbar className="justify-content-md-center" expand="lg" variant="light" bg="dark">
+        <Navbar.Brand href="/event">Event Name</Navbar.Brand>
+      </Navbar>
+
+      
       <h1>Team List</h1>
+
+      <Team name="BOBb" />
+      
+
     </div>
 
   );
