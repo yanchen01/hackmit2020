d79846681c3a31adb9e2f73159e7d61e7ff9d9d9
Christina <christina.riachi@mail.mcgill.ca>
2020-09-19 19:18:51 -0400

    added getEventByID method

--- src/backend/Events/Events.js
+++ src/backend/Events/Events.js
@@ -1,62 +1,84 @@
-import firebase from 'firebase';
+import firebase from "firebase";
 
-const addEvent = (name, eventID, eventCode, date, location = '', currentUserUID, link = '', category = '') => {
-	firebase
-		.firestore()
-		.collection('events')
-		.doc(eventID)
-		.set({
-			name,
-			eventCode,
-			date,
-			location,
-			organizer: currentUserUID,
-			link,
-			category,
-			members: [],
-			teams: []
-		})
-		.then((result) => {
-			console.log(result);
-			return result;
-		})
-		.catch((err) => {
-			console.log(err);
-		});
+const addEvent = (
+  name,
+  eventID,
+  eventCode,
+  date,
+  location = "",
+  currentUserUID,
+  link = "",
+  category = ""
+) => {
+  firebase
+    .firestore()
+    .collection("events")
+    .doc(eventID)
+    .set({
+      name,
+      eventCode,
+      date,
+      location,
+      organizer: currentUserUID,
+      link,
+      category,
+      members: [],
+      teams: [],
+    })
+    .then((result) => {
+      console.log(result);
+      return result;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 const editEvent = (eventID, updateObj) => {
-	firebase
-		.firestore()
-		.collection('events')
-		.doc(eventID)
-		.update({
-			updateObj
-		})
-		.then((result) => {
-			console.log(result);
-		})
-		.catch((err) => {
-			console.log(err);
-		});
+  firebase
+    .firestore()
+    .collection("events")
+    .doc(eventID)
+    .update({
+      updateObj,
+    })
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 const addEventMember = (eventID, memberUID) => {
-	firebase
-		.firestore()
-		.collection('events')
-		.doc(eventID)
-		.update({
-			members: firebase.firestore.FieldValue.arrayUnion(memberUID)
-		})
-		.then((result) => {
-			console.log(result);
-		})
-		.catch((err) => {
-			console.log(err);
-		});
+  firebase
+    .firestore()
+    .collection("events")
+    .doc(eventID)
+    .update({
+      members: firebase.firestore.FieldValue.arrayUnion(memberUID),
+    })
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
+const getEventById = (eventID) => {
+  firebase
+    .firestore()
+    .collection("events")
+    .doc(eventID)
+    .get()
+    .then((response) => {
+      return response;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 // const addEventTeam = (event_id, teamUID) => {
 // 	firebase
 // 		.firestore()
