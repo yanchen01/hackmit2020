60d94856eb493c9e9d288fae46e68edf04362bd7
Reid Miller <reid.miller@wisc.edu>
2020-09-19 11:29:21 -0500

    Profile Page Start/Place Holder

--- src/containers/Profile/Profile.jsx
+++ src/containers/Profile/Profile.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import { Navbar, Container, Row, Col, ListGroup, Button, Image } from 'react-bootstrap';
+import faker from 'faker';
+import './index.css';
+
+
 
 const Profile = () => {
   return (
-    <div>
-      <h1>My Profile Page</h1>
-    </div>
+    <Container className="w-100 h-100">
+        <Row className="justify-content-md-center">
+            <h1>{faker.name.firstName()}</h1>
+        </Row>
+        <Row className="justify-content-md-center">
+            <Image src={faker.image.avatar()} roundedCircle />
+        </Row>
+        
+    </Container>
+    
+
   );
 }
 
