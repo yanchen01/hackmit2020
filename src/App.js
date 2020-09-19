7b86e67710130b262a8825401a8299b0d8aba474
yanchen01 <ychen01@bu.edu>
2020-09-18 22:33:01 -0400

    set up firebase auth and connect to react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import firebase from 'firebase';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+/* Import containers */
+import Home from './containers/Home/Home';
+import Login from './components/Auth/Login';
+import SignUp from './components/Auth/SignUp';
+
+/* Import Auth */
+import { AuthProvider } from './Auth';
+import PrivateRoute from './PrivateRoute';
+
+const firebaseConfig = {
+	apiKey: 'AIzaSyD-w8z8ollWZttoETyFL7Zkk-FVnhZoKdw',
+	authDomain: 'hackmit2020-a3fb0.firebaseapp.com',
+	databaseURL: 'https://hackmit2020-a3fb0.firebaseio.com',
+	projectId: 'hackmit2020-a3fb0',
+	storageBucket: 'hackmit2020-a3fb0.appspot.com',
+	messagingSenderId: '295002459820',
+	appId: '1:295002459820:web:4f63a761bb6e0d1339d909'
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const App = () => {
+	return (
+		<AuthProvider>
+			<Router>
+				<div>
+					<Route exact path="/login" component={Login} />
+					<PrivateRoute exact path="/" component={Home} />
+					<Route exact path="/signup" component={SignUp} />
+				</div>
+			</Router>
+		</AuthProvider>
+	);
+};
 
 export default App;
