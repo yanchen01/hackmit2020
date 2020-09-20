47fdd07884a59e19b295c4cf72780e7fa51c9f8c
Reid Miller <reid.miller@wisc.edu>
2020-09-19 23:31:25 -0500

    Home Page Nav Bar

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,13 +1,19 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { Container, Button, Row, Navbar, Image, Nav } from 'react-bootstrap';
+import { Container, Button, Row, Navbar, Image, Nav, Col } from 'react-bootstrap';
 import 'react-datepicker/dist/react-datepicker.css';
 import { Redirect, useHistory } from 'react-router-dom';
 import firebase from 'firebase';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import { AuthContext } from '../../Auth';
 
 import 'react-bootstrap-typeahead/css/Typeahead.css';
 import logo from '../../assets/logo-t.png';
+
+// Drawer/HAMBURGER menu
+import { makeStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
 
 import { GitHub, Menu } from '@material-ui/icons';
 
@@ -45,21 +51,78 @@ const Home = () => {
 			});
 	};
 
+	// Hamburger
+	const [ anchor, setAnchor ] = useState(false);
+
+	const toggleDrawer = (anchor, open) => (event) => {
+		if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+			return;
+		}
+
+		setAnchor(open);
+	};
+
+	const classes = makeStyles({
+		list: {
+			width: 550
+		},
+		fullList: {
+			width: 'auto'
+		}
+	});
+
+	const list = (anchor) => (
+		<div
+			className="dr"
+			role="presentation"
+			onClick={toggleDrawer(anchor, false)}
+			onKeyDown={toggleDrawer(anchor, false)}
+		>
+			<List>
+				<Container fluid>
+					<Row>
+						<Col>
+							<h2 className="text-center">Lobby</h2>
+						</Col>
+					</Row>
+					<Row>
+						<Col>
+							<hr />
+							<h3>Current Events:</h3>
+							<ul>
+								<li>Standford Hacks 2020</li>
+								<li>Cake Cooking Competition</li>
+								<li>FizzBuzz Showdown</li>
+							</ul>
+						</Col>
+					</Row>
+					<Row>
+						<Col>
+							<hr />
+							<p>
+								<a href="/settings"> Settings</a>
+							</p>
+							<p>Sign Out</p>
+						</Col>
+					</Row>
+				</Container>
+			</List>
+		</div>
+	);
+
+	// ^^^^^^^^^^^^^
+
 	return (
 		<section className="create-event-page">
-			<Navbar bg="light" varient="light">
-				<Navbar.Brand href="/">
-					<Image src={logo} className="logo" />
-					Lobby
-				</Navbar.Brand>
-				<Navbar.Collapse className="justify-content-end">
-					<Nav>
-						<Nav.Link href="https://github.com/yanchen01/hackmit2020">
-							<GitHub />
-						</Nav.Link>
-					</Nav>
-				</Navbar.Collapse>
+			<Navbar className="" expand="lg" variant="light" bg="light">
+				<FontAwesomeIcon onClick={toggleDrawer('left', true)} className="" icon="hamburger" />
+				<Navbar.Brand href="/" className="mx-auto"><Image src={logo} className="logo" />Lobby</Navbar.Brand>
 			</Navbar>
+			<React.Fragment key="left">
+				<Drawer anchor="left" open={anchor} onClose={toggleDrawer('left', false)}>
+					{list(anchor)}
+				</Drawer>
+			</React.Fragment>
 			<Container className="my-5">
 				<h1>
 					{user && `Welcome ${user}, `}
