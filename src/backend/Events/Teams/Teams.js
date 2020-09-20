17683344cdfd5321ddf3ca5281c72c71998e7a44
yanchen01 <ychen01@bu.edu>
2020-09-19 21:03:58 -0400

    retrieved teams and events

--- src/backend/Events/Teams/Teams.js
+++ src/backend/Events/Teams/Teams.js
@@ -1,103 +1,95 @@
-import firebase from "firebase";
+import firebase from 'firebase';
 
 // dont know how to implement authorization for these methods to check if the user calling them is allowed to
 
 const updateTeam = (teamId, teamName, capacity, teamTags) => {
-  firebase
-    .firestore()
-    .collection("teams")
-    .doc(teamId)
-    .update({
-      teamName: teamName,
-      capacity: capacity,
-      teamTags: teamTags,
-    })
-    .then(() => {})
-    .catch((error) => console.log(error));
+	firebase
+		.firestore()
+		.collection('teams')
+		.doc(teamId)
+		.update({
+			teamName: teamName,
+			capacity: capacity,
+			teamTags: teamTags
+		})
+		.then(() => {})
+		.catch((error) => console.log(error));
 };
 
 // no need to remove an application, we can keep it in the history of applications and just mark it as rejected
 const addApplicationToTeam = (teamId, applicationId) => {
-  firebase
-    .firestore()
-    .collection("teams")
-    .doc(teamId)
-    .update({
-      applications: firebase.firestore.FieldValue.arrayUnion(applicationId),
-    })
-    .then(() => {})
-    .catch((error) => console.log(error));
+	firebase
+		.firestore()
+		.collection('teams')
+		.doc(teamId)
+		.update({
+			applications: firebase.firestore.FieldValue.arrayUnion(applicationId)
+		})
+		.then(() => {})
+		.catch((error) => console.log(error));
 };
 
 const addMemberToTeam = (teamId, newMemberId) => {
-  firebase
-    .firestore()
-    .collection("teams")
-    .doc(teamId)
-    .update({
-      members: firebase.firestore.FieldValue.arrayUnion(newMemberId),
-    })
-    .then(() => {})
-    .catch((error) => console.log(error));
+	firebase
+		.firestore()
+		.collection('teams')
+		.doc(teamId)
+		.update({
+			members: firebase.firestore.FieldValue.arrayUnion(newMemberId)
+		})
+		.then(() => {})
+		.catch((error) => console.log(error));
 };
 
 const removeMemberFromTeam = (teamId, removedMemberId) => {
-  firebase
-    .firestore()
-    .collection("teams")
-    .doc(teamId)
-    .update({
-      members: firebase.firestore.FieldValue.arrayRemove(removedMemberId),
-    })
-    .then(() => {})
-    .catch((error) => console.log(error));
+	firebase
+		.firestore()
+		.collection('teams')
+		.doc(teamId)
+		.update({
+			members: firebase.firestore.FieldValue.arrayRemove(removedMemberId)
+		})
+		.then(() => {})
+		.catch((error) => console.log(error));
 };
 
-const createTeam = (
-  teamId,
-  teamName,
-  eventId,
-  members,
-  capacity,
-  isFull,
-  applications,
-  teamTags
-) =>
-  firebase
-    .firestore()
-    .collection("teams")
-    .doc(teamId)
-    .set({
-      name: teamName,
-      eventId: eventId,
-      members: members,
-      capacity: capacity,
-      isFull: isFull,
-      teamTags: teamTags,
-      applications: applications,
-    })
-    .then((result) => {
-      return result;
-    })
-    .catch((error) => console.log(error));
+const createTeam = (teamId, teamName, eventId, members, capacity, isFull, applications, teamTags) =>
+	firebase
+		.firestore()
+		.collection('teams')
+		.doc(teamId)
+		.set({
+			name: teamName,
+			eventId: eventId,
+			members: members,
+			capacity: capacity,
+			isFull: isFull,
+			teamTags: teamTags,
+			applications: applications
+		})
+		.then((result) => {
+			return result;
+		})
+		.catch((error) => console.log(error));
 
-const getAllTeamsInEvent = (eventId) =>
-  firebase
-    .firestore()
-    .collection("teams")
-    .where("eventId", "==", eventId)
-    .get()
-    .then((querySnapshot) => {
-      console.log(querySnapshot)
-      // return querySnapshot;
-    })
-    .catch((error) => console.log(error));
+const getAllTeamsInEvent = (eventId) => {
+	const teams = [];
 
-export {
-  createTeam,
-  getAllTeamsInEvent,
-  removeMemberFromTeam,
-  addMemberToTeam,
-  addApplicationToTeam,
-  updateTeam,
+	firebase
+		.firestore()
+		.collection('teams')
+		.where('eventId', '==', eventId)
+		.get()
+		.then((querySnapshot) => {
+			querySnapshot.forEach((element) => {
+				teams.push(element.data());
+			});
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+
+	return teams;
 };
+
+export { createTeam, getAllTeamsInEvent, removeMemberFromTeam, addMemberToTeam, addApplicationToTeam, updateTeam };
--- src/containers/TeamList/index.jsx
+++ src/containers/TeamList/index.jsx
@@ -1,192 +1,193 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Navbar, Container, Row, Col, ListGroup, Button, Modal } from 'react-bootstrap';
 import InfiniteScroll from 'react-infinite-scroll-component';
-import faker from 'faker'
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import faker from 'faker';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './index.css';
-import {getAllTeamsInEvent} from '../../backend/Events/Teams/Teams';
+import { getAllTeamsInEvent } from '../../backend/Events/Teams/Teams';
 
 // Drawer/HAMBURGER menu
 import { makeStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 import List from '@material-ui/core/List';
 
-
 const TeamList = () => {
+	// Hamburger
+	function EventModal(props) {
+		return (
+			<Modal {...props} size="lg" aria-labelledby="contained-modal-title-vcenter" centered>
+				<Modal.Body>
+					<p>
+						<h3>Welcome to {props.eventName}!</h3>
+					</p>
+					<p>{props.description}</p>
+					<h3> Conect With Us: </h3>
+					<ul>
+						<li>
+							<a href={props.link}>{props.link}</a>
+						</li>
+						<li>Event Code: {props.eventCode}</li>
+						<li>Location: {props.location}</li>
+						<li>Category: {props.category}</li>
+					</ul>
+					<Button onClick={props.onHide}>Close</Button>
+				</Modal.Body>
+			</Modal>
+		);
+	}
 
-  // Hamburger
-  function EventModal(props) {
-    console.log(getAllTeamsInEvent("5bdd5ee5-5f7b-42af-a909-ec98d39becd7"));
-    return (
-      <Modal
-        {...props}
-        size="lg"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
-      >
-        <Modal.Body>
-          <p><h3>Welcome to {props.eventName}!</h3></p>
-          <p>{props.description}</p>
-          <h3> Conect With Us: </h3>
-          <ul>
-            <li><a href={props.link}>{props.link}</a></li>
-            <li>Event Code: {props.eventCode}</li>
-            <li>Location: {props.location}</li>
-            <li>Category: {props.category}</li>
-          </ul>
-          <Button onClick={props.onHide}>Close</Button>
-        </Modal.Body>
+	const [ anchor, setAnchor ] = useState(false);
 
-      </Modal>
-    );
-  }
+	useEffect(() => {
+		const teams = getAllTeamsInEvent('5bdd5ee5-5f7b-42af-a909-ec98d39becd7');
+		console.log('Result: ', teams);
+	}, []);
 
-  const [anchor, setAnchor] = useState(false);
+	const toggleDrawer = (anchor, open) => (event) => {
+		if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+			return;
+		}
 
-  const toggleDrawer = (anchor, open) => (event) => {
-    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
-      return;
-    }
+		setAnchor(open);
+	};
 
-    setAnchor(open);
-  };
+	const classes = makeStyles({
+		list: {
+			width: 550
+		},
+		fullList: {
+			width: 'auto'
+		}
+	});
 
-  const classes = makeStyles({
-    list: {
-      width: 550,
-    },
-    fullList: {
-      width: 'auto',
-    },
-  });
+	const list = (anchor) => (
+		<div
+			className="dr"
+			role="presentation"
+			onClick={toggleDrawer(anchor, false)}
+			onKeyDown={toggleDrawer(anchor, false)}
+		>
+			<List>
+				<Container fluid>
+					<Row>
+						<Col>
+							<h2 className="text-center">Lobby</h2>
+						</Col>
+					</Row>
+					<Row>
+						<Col>
+							<hr />
+							<h3>Current Events:</h3>
+							<ul>
+								<li>Standford Hacks 2020</li>
+								<li>Cake Cooking Competition</li>
+								<li>FizzBuzz Showdown</li>
+							</ul>
+						</Col>
+					</Row>
+					<Row>
+						<Col>
+							<hr />
+							<p>
+								<a href="/settings">
+									<FontAwesomeIcon icon="cog" /> Settings
+								</a>
+							</p>
+							<p>
+								<FontAwesomeIcon icon="sign-out-alt" /> Sign Out
+							</p>
+						</Col>
+					</Row>
+				</Container>
+			</List>
+		</div>
+	);
 
-  const list = (anchor) => (
-    
-    <div
-      className="dr"
-      role="presentation"
-      onClick={toggleDrawer(anchor, false)}
-      onKeyDown={toggleDrawer(anchor, false)}
-    >
-    
-    <List>
-      <Container fluid>
-        <Row>
-          <Col>
-            <h2 className="text-center">Lobby</h2>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-          <hr/>
-          <h3>Current Events:</h3>
-          <ul>
-            <li>Standford Hacks 2020</li>
-            <li>Cake Cooking Competition</li>
-            <li>FizzBuzz Showdown</li>
-          </ul>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <hr/>
-            <p><a href="/settings"><FontAwesomeIcon icon="cog" /> Settings</a></p>
-            <p><FontAwesomeIcon icon="sign-out-alt" /> Sign Out</p>
-          </Col>
-        </Row>
-      </Container>
-    </List>
-    
-    
-    </div>
-    
-  );
+	const [ modalShow, setModalShow ] = React.useState(false);
 
-  const [modalShow, setModalShow] = React.useState(false);
+	// ^^^^^^^^^^^^^
 
+	const data = new Array(1000).fill().map((value, id) => ({
+		id: id,
+		title: faker.name.firstName(),
+		body: faker.lorem.sentences(8)
+	}));
 
-  // ^^^^^^^^^^^^^
+	const [ count, setCount ] = useState({
+		prev: 0,
+		next: 10
+	});
 
-  const data = new Array(1000).fill().map((value, id) => (({
-    id: id,
-    title: faker.name.firstName(),
-    body: faker.lorem.sentences(8)
-  })))
+	const [ hasMore, setHasMore ] = useState(true);
+	const [ current, setCurrent ] = useState(data.slice(count.prev, count.next));
+	const getMoreData = () => {
+		if (current.length === data.length) {
+			setHasMore(false);
+			return;
+		}
+		setTimeout(() => {
+			setCurrent(current.concat(data.slice(count.prev + 10, count.next + 10)));
+		}, 2000);
+		setCount((prevState) => ({ prev: prevState.prev + 10, next: prevState.next + 10 }));
+	};
 
-  const [count, setCount] = useState({
-    prev: 0,
-    next: 10
-  })
+	return (
+		<div>
+			<Navbar className="" expand="lg" variant="light" bg="light">
+				<FontAwesomeIcon onClick={toggleDrawer('left', true)} className="" icon="hamburger" />
+				<Navbar.Brand className="mx-auto" onClick={() => setModalShow(true)}>
+					HackMIT2020
+				</Navbar.Brand>
+			</Navbar>
+			<EventModal
+				show={modalShow}
+				onHide={() => setModalShow(false)}
+				eventName="HackMIT 2020"
+				description={faker.lorem.words(20)}
+				link="https://hackmit.org/"
+				eventCode="HackWithUs"
+				location="The World Wide Web"
+				category="GeekSquad"
+				eventStart="9/18"
+				eventEnd="9/21"
+			/>
+			<br />
+			<h3 className="text-center">Find your Team:</h3>
 
-  const [hasMore, setHasMore] = useState(true);
-  const [current, setCurrent] = useState(data.slice(count.prev, count.next))
-  const getMoreData = () => {
-    if (current.length === data.length) {
-      setHasMore(false);
-      return;
-    }
-    setTimeout(() => {
-      setCurrent(current.concat(data.slice(count.prev + 10, count.next + 10)))
-    }, 2000)
-    setCount((prevState) => ({ prev: prevState.prev + 10, next: prevState.next + 10 }))
-  }
+			<React.Fragment key="left">
+				<Drawer anchor="left" open={anchor} onClose={toggleDrawer('left', false)}>
+					{list(anchor)}
+				</Drawer>
+			</React.Fragment>
 
-  return (
-    
-    <div>
-      <Navbar className="" expand="lg" variant="light" bg="light">
-        <FontAwesomeIcon onClick={toggleDrawer("left", true)} className="" icon="hamburger" />
-        <Navbar.Brand className="mx-auto" onClick={() => setModalShow(true)}>HackMIT2020</Navbar.Brand>
-      </Navbar>
-      <EventModal
-        show={modalShow}
-        onHide={() => setModalShow(false)}
-        eventName="HackMIT 2020"
-        description={faker.lorem.words(20)}
-        link="https://hackmit.org/"
-        eventCode="HackWithUs"
-        location="The World Wide Web"
-        category="GeekSquad"
-        eventStart="9/18"
-        eventEnd="9/21"
-      />
-      <br />
-      <h3 className="text-center">Find your Team:</h3>
-      
-      <React.Fragment key="left">
-        <Drawer anchor="left" open={anchor} onClose={toggleDrawer("left", false)}>
-          {list(anchor)}
-        </Drawer>
-      </React.Fragment>
+			<InfiniteScroll
+				dataLength={current.length}
+				next={getMoreData}
+				hasMore={hasMore}
+				loader={<h2 className="text-center">Finding More Awesome Teams...</h2>}
+			>
+				<ListGroup variant="flush">
+					{current &&
+						current.map((item, index) => (
+							<ListGroup.Item key={index} className="mt-3 post">
+								<Row className="align-items-center">
+									<Col sm={10}>
+										<h3>{`${item.title}`}</h3>
+										<p>{item.body}</p>
+									</Col>
+									<Col sm={2}>
+										<h4>3/4</h4>
+										<Button size="lg" variant="outline-primary">
+											Join <FontAwesomeIcon icon="arrow-right" />
+										</Button>
+									</Col>
+								</Row>
+							</ListGroup.Item>
+						))}
+				</ListGroup>
+			</InfiniteScroll>
+		</div>
+	);
+};
 
-      <InfiniteScroll
-        dataLength={current.length}
-        next={getMoreData}
-        hasMore={hasMore}
-        loader={<h2 className="text-center">Finding More Awesome Teams...</h2>}
-      >
-      
-        <ListGroup variant="flush">
-          {current && current.map(((item, index) => (
-            <ListGroup.Item key={index} className="mt-3 post">
-              <Row className="align-items-center">
-                <Col sm={10}><h3>{`${item.title}`}</h3>
-                <p>{item.body}</p>
-                </Col>
-                <Col sm={2}>
-                  <h4>3/4</h4>
-                  <Button size="lg" variant="outline-primary">Join <FontAwesomeIcon icon="arrow-right" /></Button>
-                </Col>
-              </Row>
-            </ListGroup.Item>
-          )))
-          }
-        </ListGroup>
-      </InfiniteScroll>
-    </div>
-  );
-}
-
-
-  
 export default TeamList;
